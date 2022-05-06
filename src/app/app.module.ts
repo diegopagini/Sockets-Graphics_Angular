2105cb62750355b2e5ec377a2a43ac6df16768d1
Diego Pagini <diego.pagini@neoris.com>
2022-05-06 14:40:03 +0200

    Graphics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+const config: SocketIoConfig = {
+  url: environment.wsUrl,
+};
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SocketIoModule.forRoot(config)],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
